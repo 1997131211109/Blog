9b6ddedfc32ed2dfc8375442fdae18c953b4036f
denys <petrenckoden215@gmail.com>
2023-06-07 10:33:27 +0300

    Creat 2 (start to work) , update 3. text2

--- redme.update.py
+++ redme.update.py
@@ -9,9 +9,11 @@
 # We transfer the data with the comit, what we did in the project to the "github"
 
 # 2. We create brunch "1.2 , for the For testing and improvement"
-# We continue to work on our topic, our task is to show how branches work separately from the project, and then transfer the results to the project of the previous branch
+# We continue to work on our topic, our task is to show how branches work separately from the project
+# Then transfer the results to the project of the previous branch
 # If our data is correct, then we can move it to the main branch of the project: main
 
 # 3. Create a branch transfer to the main project
-# To do this, we need to select the changes that we would like to transfer between branches for further transfer to the main branch
+# To do this, we need to select the changes that
+# We would like to transfer between branches for further transfer to the main branch
 #
