ce0887375a511fbec4036f9bdf43fc931475feb3
denys <petrenckoden215@gmail.com>
2023-06-07 10:00:33 +0300

    Creat 1 (start to work)

--- redme.update.py
+++ redme.update.py
@@ -2,4 +2,8 @@
 # Petrenko Denys In.m - 26
 # Release Branching Strategy
 
-#
+# 1.  We implement the scheme  Git Flow
+# 1.1 We create brunch "1.0 , for the developer"
+# To do this, we need to use the command in the console  "git checkout -b featur/1.0"
+# Go to the created branch, and create a fail "readme.update.py"
+# We transfer the data with the comit, what we did in the project to the "github"
