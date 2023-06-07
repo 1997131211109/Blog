550290e9189979b8f9a5704f11c4e7615e0aa077
denys <petrenckoden215@gmail.com>
2023-06-07 10:22:41 +0300

    Creat 2 (start to work) , Correct 2. text

--- redme.update.py
+++ redme.update.py
@@ -8,8 +8,7 @@
 # Go to the created branch, and create a fail "readme.update.py"
 # We transfer the data with the comit, what we did in the project to the "github"
 
-# 2. Create a branch 1.1 in which we will describe the events further
-# We create brunch "1.2 , for the For testing and improvement"
+# 2. We create brunch "1.2 , for the For testing and improvement"
 # We continue to work on our topic, our task is to show how branches work separately from the project, and then transfer the results to the project of the previous branch
 # If our data is correct, then we can move it to the main branch of the project: main
 
